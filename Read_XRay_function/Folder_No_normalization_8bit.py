4ed20f124b21ea2940d77447f6f66132f8d13d9b
Chuanyi <4a539035@stust.edu.tw>
2021-07-19 23:31:37 +0800

    add_8bit_image

--- Read_XRay_function/Folder_No_normalization_8bit.py
+++ Read_XRay_function/Folder_No_normalization_8bit.py
@@ -2,8 +2,8 @@ import numpy as np
 import pydicom
 from pydicom.pixel_data_handlers.util import apply_voi_lut
 import cv2
-
-path="path"
+import os
+yourPath = 'C:/Hospital_Project/Image_preprocessing/images/DICOM/'
 def read_xray(path, voi_lut = True, fix_monochrome = True):
     input_dicom = pydicom.read_file(path)
     print(input_dicom.pixel_array.shape)
@@ -24,7 +24,12 @@ def image_16bit_to_8bit(type_16bit):
     image_8bit = np.array(np.rint(255 * ((type_16bit - min_16bit) / (max_16bit - min_16bit))), dtype=np.uint8)
     return image_8bit
 
+allFileList = os.listdir(yourPath)
+for file in allFileList:
+  if os.path.isdir(os.path.join(yourPath,file)):
+    print("I'm a directory: " + file)
 
-img = read_xray(path)
-img=image_16bit_to_8bit(img)
-cv2.imwrite('output.png', img,[cv2.IMWRITE_PNG_COMPRESSION, 0])
+  elif os.path.isfile(yourPath+file):
+    img = read_xray(yourPath+file)
+    img=image_16bit_to_8bit(img)
+    cv2.imwrite(file[:-4]+'.png', img,[cv2.IMWRITE_PNG_COMPRESSION, 0])
